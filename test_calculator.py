b233e29b101f90d13629636bd790566caa6269b2
NicolasKuban <nicrad@yandex.ru>
2022-09-11 12:36:18 +0300

    Переработаны функции классов, при объявлении функций забыл вставить ключевое слово self. В фукнция обращение к значениям параметров идет через ключевое слово self. Проведены все тесты.

--- test_calculator.py
+++ test_calculator.py
@@ -4,10 +4,11 @@
 #
 # Author:      Пользователь
 #
-# Created:     08.09.2022
+# Created:     11.09.2022
 # Copyright:   (c) Пользователь 2022
 # Licence:     <your licence>
 #-------------------------------------------------------------------------------
+
 
 import datetime as dt
 
@@ -15,10 +16,13 @@ USD_RATE = 60.70
 EURO_RATE = 60.80
 date_format = '%d.%m.%Y'
 
+print(dt.datetime.now().date())
+#limit = 1000
+
 class Record:
     def __init__(self, amount, date, comment):
         self.amount = amount
-        self.date =  dt.datetime.strptime(date, date_format)
+        self.date =  dt.datetime.strptime(date, date_format)  # перевести в тип данных дата?
         self.comment = comment
 
 class Calculator:
@@ -31,6 +35,7 @@ class Calculator:
         today = dt.datetime.now().date()
         today_stats = 0
         for record in self.records:
+#            day_of_record = dt.datetime.strptime(value[1], date_format).date()
             if today == record.date.date():
                 today_stats += record.amount
         return today_stats
@@ -47,6 +52,7 @@ class Calculator:
     def add_record(self, record):
         self.records.append(record)
 
+
 class CaloriesCalculator(Calculator):
     def get_calories_remained(self):
         calories_received = self.get_today_stats()
@@ -62,6 +68,8 @@ class CashCalculator(Calculator):
                     "usd":[USD_RATE, "USD"],
                     "eur":[EURO_RATE, "Euro"]
         }
+        # Округлять до сотых долей
+        # balans = limit - sum(records.values())
         today_cash_spent = self.get_today_stats()
         today_cash_remained = self.limit - today_cash_spent
         balans_valute = round(today_cash_remained / valute[currency][0], 2)
@@ -74,3 +82,31 @@ class CashCalculator(Calculator):
             return "Денег нет, держись: твой долг - " + balans_string
         else:
             return "Денег нет, держись"
+
+
+
+cash_calc = CashCalculator(2222)
+r5 = Record(amount=23, comment="Безудержный шопинг", date="09.09.2022")
+r6 = Record(amount=111, comment="Наполнение потребительской корзины", date="11.09.2022")
+r7 = Record(amount=2222, comment="Катание на такси", date="11.09.2022")
+cash_calc.add_record(r5)
+cash_calc.add_record(r6)
+cash_calc.add_record(r7)
+print(cash_calc.records[0].comment)
+print(cash_calc.get_week_stats())
+print(cash_calc.get_today_stats())
+print(cash_calc.get_today_cash_remained("rub"))
+print(cash_calc.get_today_cash_remained("usd"))
+print(cash_calc.get_today_cash_remained("eur"))
+
+calcal = CaloriesCalculator(2200)
+r14 = Record(amount=1186, comment="Кусок тортика. И ещё один.", date="11.09.2022")
+r15 = Record(amount=84, comment="Йогурт.", date="10.09.2022")
+r16 = Record(amount=1140, comment="Баночка чипсов.", date="01.09.2022")
+for r in [r14, r15, r16]:
+    calcal.add_record(r)
+
+print(calcal.records[0].comment)
+print(calcal.get_week_stats())
+print(calcal.get_today_stats())
+print(calcal.get_calories_remained())
